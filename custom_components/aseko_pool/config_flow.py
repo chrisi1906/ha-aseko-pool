dd3b93aba7ed462a62bafd2b84f3c80b79ffb947
chrisi1906 <christoph.kerschbaumer@live.at>
2026-08-05 06:25:28 +0200

    Add config flow for Aseko Pool integration

---
+++ custom_components/aseko_pool/config_flow.py
@@ -0,0 +1,31 @@
+"""Config flow for the Aseko Pool integration."""
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+
+from .const import DOMAIN
+
+
+class AsekoPoolConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Aseko Pool."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Aseko Pool",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required("api_key"): str,
+                }
+            ),
+        )
