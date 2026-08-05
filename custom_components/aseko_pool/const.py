061de230fc86efe37062f30c6f694c161761f232
chrisi1906 <christoph.kerschbaumer@live.at>
2026-08-05 06:23:48 +0200

    Add constants for Aseko Pool integration

---
+++ custom_components/aseko_pool/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Aseko Pool integration."""
+
+DOMAIN = "aseko_pool"
+NAME = "Aseko Pool"
+
+API_URL = "https://api.aseko.cloud/api/v1"
+
+CLIENT_NAME = "Home Assistant Aseko"
+CLIENT_VERSION = "0.1.0"
